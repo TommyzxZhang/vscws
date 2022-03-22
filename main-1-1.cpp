5865da7eeaaa0af3e11e19b14821aa1f681f747f
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-03-22 13:34:01 +0800

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+extern void copy_2d_strings(std::string first[][2], std::string second[][2], int n);
+
+
+int main(){
+    int n = 3;
+    std::string first[3][2] = {{"a","a",},{"a","a"},{"a","a"} };
+    std::string second[3][2];
+    void copy_2d_strings(std::string first[][2], std::string second[][2], int n);
+}
