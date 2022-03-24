5f134d4c074a12a8f76a98b129b6b9ced91e4bdc
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-03-24 23:14:57 +0800

    Add files via upload

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-void cpyda(double *old_array, double * new_array, int length) {
-	for (int i = 0; i < length; old_array++,new_array++) {
-		* new_array = *old_array;
+void cpyda(double *old_array, double *new_array, int length) {
+	double *a, *b;
+	a = old_array;
+	b = new_array;
+	for (int i = 0; i < length; i++, a++,b++) {
+		*b = *a;
 	}
-	
 }
