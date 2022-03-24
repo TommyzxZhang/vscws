21d774840f741d5b3a2a525dc120602abb0ef447
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-03-24 15:44:20 +0800

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 void copy_2d_strings(std::string first[][2], std::string second[][2], int n){
+
     for (int i = 0; i < 2; i++){
         for (int j = 0; j < n; j++){
-            second[i][j]=first[i][j];
+            second[i][j] = first[i][j];
         }
     }
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 extern void copy_2d_strings(std::string first[][2], std::string second[][2], int n);
+using namespace std;
 
 
 int main(){
     int n = 3;
-    std::string first[3][2] = {{"a","a",},{"a","a"},{"a","a"} };
+    std::string first[3][2] = {{"1","2",},{"1","2"},{"1","2"} };
     std::string second[3][2];
-    void copy_2d_strings(std::string first[][2], std::string second[][2], int n);
+    copy_2d_strings(first, second, n);
 }
