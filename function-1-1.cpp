45a2b15a0eb53332c700f333788460d1bca75d92
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-03-24 15:48:07 +0800

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 void copy_2d_strings(std::string first[][2], std::string second[][2], int n){
 
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < 2; j++){
             second[i][j] = first[i][j];
         }
     }
