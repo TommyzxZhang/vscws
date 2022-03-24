9bd3de2121c04b6913f3c8f570845499fde8506c
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-03-24 17:46:11 +0800

    Delete main-1-4.cpp

--- main-1-4.cpp
+++
@@ -1,9 +0,0 @@
-extern void cpyda(double* old_array, double* new_array, int length);
-
-int main() {
-	int length = 10;
-	double *a, old_array[10] = { 1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,0.0 };
-	double *b, new_array[10];
-	cpyda(old_array, new_array, length);
-	delete *old_array;
-}
