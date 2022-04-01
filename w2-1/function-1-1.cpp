21f64d5ff133a7d2b1147c643c2151a56bac43a0
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-04-01 12:40:20 +0800

    Delete w2-1 directory

--- w2-1/function-1-1.cpp
+++
@@ -1,8 +0,0 @@
-
-int diagonal(int array[4][4]){
-    int sum = 0;
-    for (int i = 0; i < 3; i++){
-        sum = sum + array[i][i];
-    }
-    return flag;
-}
--- w2-1/main-1-1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-extern int diagonal(int array[4][4]);
-int main(){
-    int array[4][4] = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
-    std::cout<< diagonal(array)<<std::endl;
-}
