567f1339b49abac9cc67895262ff6d6e11801c87
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-04-01 13:47:55 +0800

    Add files via upload

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -6,8 +6,13 @@ extern int* count_numbers_in_range(int vals[], int length, int bottom, int top);
 int main() {
 	int vals[] = { 1,7,3,4,6,5,2 };
 	int *res = count_numbers_in_range(vals, 7, 3, 6);
-
+	for (int i = 0; i < 4; i++){
+		cout << *(res + i);
+	}
 
 	int vals2[] = { 1,4,5,6,8,2,9,4,2,5,6,1 };
-	int *res2 = count_numbers_in_range(vals, 12, 3, 6);
+	int *res2 = count_numbers_in_range(vals2, 12, 3, 6);
+	for (int i = 0; i < 6; i++) {
+		cout << *(res2 + i);
+	}
 }
