1fd2899e9f632c8eba238509184958e611b8bcca
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-04-01 13:55:38 +0800

    Add files via upload

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,11 +1,10 @@
-int* count_numbers_in_range(int vals[], int length, int bottom, int top) {
+int count_numbers_in_range(int vals[], int length, int bottom, int top) {
 	int *arr = new int[length];
 	int k = 0;
 	for (int i = 0; i < length; i++){
 		if (bottom <= vals[i] && vals[i] <= top) {
-			arr[k] = vals[i];
 			k++;
 		}
 	}
-	return arr;
+	return k;
 }
