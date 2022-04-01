ef655171326d4c255fbbf3a61c809dd7547f1adc
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-04-01 12:40:00 +0800

    Delete w1-2 directory

--- w1-2/main-2-2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-extern int maximum(int array[], int n);
-
-int main(){
-    int array[7] = {4,5,6,7,8,9,11};
-    std::cout << "The number is: " << maximum(array, 7) << std::endl;
-}
--- w1-2/main-2-5.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-extern bool descending(int array[], int n);
-int main(){
-    int array[7] = {11,10,9,8,7,6,5};
-    std::cout << descending(array, 7) << std::endl;
-}
