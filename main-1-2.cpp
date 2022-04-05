55abd01fdc50b5bf9366a7b6a7cd2cd59dac99fa
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-04-05 15:20:03 +0800

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,18 +1,5 @@
 #include "person.h"
 
-person::person(string myName, int Salary) {
-	name = myName;
-	sala = Salary;
-}
-void person::setName(string myName) {
-	name = myName;
-}
-void person::setSalary(int mySalary) {
-	sala = mySalary;
-}
-string person::getName() {
-	return name;
-}
-int person::getSalary() {
-	return sala;
+int main() {
+	person f("fuck", 100);
 }
--- person.cpp
+++ person.cpp
@@ -1,5 +1,20 @@
+
+
 #include "person.h"
 
-int main() {
-	person f("fuck",100);
+person::person(string myName, int Salary) {
+	name = myName;
+	sala = Salary;
+}
+void person::setName(string myName) {
+	name = myName;
+}
+void person::setSalary(int mySalary) {
+	sala = mySalary;
+}
+string person::getName() {
+	return name;
+}
+int person::getSalary() {
+	return sala;
 }
