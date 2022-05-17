c44444efe03a994776089143b7ce0c546a2efc4d
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-05-17 13:54:47 +0800

    Delete main-1-1.cpp

--- main-1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-extern int* shift_right(int* vals, int len, int amount);
-
-int main() {
-	int vals[] = { 1,2,3,4,5 };
-	int *sv;
-	sv = shift_right(vals, 5, 2);
-	delete[] sv, vals;
-}
