88f406b05d04cc7cf462cf6a0721376d277ba0a3
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-05-17 13:53:48 +0800

    Delete function-1-4.cpp

--- function-1-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-void cpyda(double *old_array, double *new_array, int length) {
-	double *a, *b;
-	a = old_array;
-	b = new_array;
-	for (int i = 0; i < length; i++, a++,b++) {
-		*b = *a;
-	}
-}
