93f1e251cc0da8beca47d4c7fa8f451442640111
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-05-17 13:55:00 +0800

    Delete main-1-3.cpp

--- main-1-3.cpp
+++
@@ -1,8 +0,0 @@
-extern void cpyia(int old_array[], int new_array[], int length); 
-
-int main() {
-	int length = 10;
-	int *a, old_array[10] = { 1,2,3,4,5,6,7,8,9,0 };
-	int	*b, new_array[10];
-	cpyia(old_array, new_array, length);
-}
