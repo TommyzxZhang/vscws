740e39eabf9aaf1e512829f27c74963eb9d7bdca
TommyzxZhang <100658679+TommyzxZhang@users.noreply.github.com>
2022-06-03 14:44:36 +0800

    Add files via upload

--- AmoebaTickets.cpp
+++ AmoebaTickets.cpp
@@ -5,16 +5,14 @@ AmoebaTickets::AmoebaTickets(string n) {
     name = n;
     Amoeba(a);
     address = "";
-}  // creates the record store with name n 
-                              // also call the parent class constructor
-                              // with an empty address string.
-int AmoebaTickets::ticketsSold() {
+}
+void AmoebaTickets::ticketsSold() {
     sales = 0;
-}          // and number of tickets sold so far should be initialised to zero
+}
 
 void AmoebaTickets::sellTicket() {
     sales++;
-}        // increments the number of tickets sold  
+}  
 int AmoebaTickets::get_ticketsSold() {
     return sales;
-}        // gets the number of tickets sold
+} 
